bd2164b2c4a3b3d76e7fa8ae8d41460c0ff11df9
Daniel Liu <daniell.liu@mail.utoronto.ca>
2025-06-15 22:56:38 -0400

    added transcript

--- myscript.js
+++ myscript.js
@@ -10,10 +10,14 @@ function changePage(id)
     }else if (id == "Experience")
     {
         location.replace("experience.html")
-    }else if(id=="Projects"){
+    }else if (id=="Projects") {
         location.replace("projects.html")
-    }else{
+    } else if (id == "Resume") {
         location.replace("resume.pdf")
+    } else if (id == "Transcript") {
+        location.replace("transcript.pdf")
+    } else {
+        alert("Error: Invalid page ID. Please try again.");
     }
     
 }
